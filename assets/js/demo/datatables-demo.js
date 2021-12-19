249d5d4439cbe5a5536a99384eb937c38d4aa8d6
Facundo Rivera <facuriive@gmail.com>
2021-12-19 03:06:35 -0300

    Auto-generated commit [ci skip]

---
+++ assets/js/demo/datatables-demo.js
@@ -0,0 +1,24 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable(
+    {
+      "scrollY":        "200px",
+      "scrollCollapse": true,
+      "paging":         false
+  }
+  );
+});
+
+
+// Call the dataTables jQuery plugin
+// $(document).ready(function() {
+//   $('#dataTable2').DataTable(
+//     {
+//       "scrollY":        "200px",
+//       "scrollCollapse": true,
+//       "paging":         false
+//   }
+//   );
+// });
+
+
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
+
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA9qle7nT73YpfWdpTi1O8Lrhi3AZy9iSk",
+      authDomain: "angularpush-f0c96.firebaseapp.com",
+      projectId: "angularpush-f0c96",
+      storageBucket: "angularpush-f0c96.appspot.com",
+      messagingSenderId: "927986775613",
+      appId: "1:927986775613:web:78061c5e8d04abcb639c54"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+console.log(messaging)
+
+
+
